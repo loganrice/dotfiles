8fe59bdfbec9961b0e1ceeb023ddee51ed599b6a
Logan Rice <loganrice72@gmail.com>
2018-07-11 20:10:55 +0000

    new vimrc for neovim setup wip

--- dotfile_setup.sh
+++ dotfile_setup.sh
@@ -1,33 +1,18 @@
 #!/bin/bash
 ############################
-# .make.sh
+# dotfiles_setup.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
 ########## Variables
-
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .vimrc .vim .zshrc .oh-my-zsh"    # list of files/folders to symlink in homedir
+dotfiles=(".vimrc" ".zshrc" ".vim" ".oh-my-zsh")     # list of files/folders to symlink in homedir
+dir="${HOME}/dotfiles"                               # dotfiles directory
 
 ##########
 
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
+for dotfile in "${dotfiles[@]}";do
+ echo "Creating symlink to $dotfile in home directory $dir."
+ ln -sf "${dir}/${dotfile}" "${HOME}/${dotfile}" 
 done
 
 install_zsh () {
